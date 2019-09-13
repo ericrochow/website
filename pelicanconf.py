532b1b25054689d60e39942bab1f5955f7b7eedf
Eric Rochow <erochow@liquidweb.com>
2019-09-13 15:23:40 -0400

    customize configuration a bit better

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
 
 AUTHOR = "Eric Rochow"
-SITENAME = "Eric Rochow"
-SITEURL = ""
-COPYRIGHT_NAME = "Eric Rochow"
+SITEURL = "https://ericroc.how"
+SITENAME = AUTHOR
+SITETITLE = AUTHOR
+# SITESUBTITLE = ""
+# SITEDESCRIPTION = ""
+SITELOGO = SITEURL + "/images/face.png"
+# FAVICON = SITEURL + ""
+COPYRIGHT_NAME = AUTHOR
+COPYRIGHT_YEAR = datetime.now().year
 
 PATH = "content"
 
 TIMEZONE = "America/Detroit"
 
 DEFAULT_LANG = "en"
+OG_LOCALE = "en_US"
+LOCALE = "en_US"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,6 +29,24 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 THEME = "Flex"
+
+# PLUGINS = [
+# "liquid_tags",
+# "pin_to_top",
+# "related_posts",
+# "similar_posts",
+# "pelican-githubprojects",
+# "video_privacy_enhancer",
+# "goodreads_activity",
+# ]
+JINJA_EXTENSIONS = []
+
+ROBOTS = "index, follow"
+STATIC_PATHS = ["images", "extra/robots.txt", "extra/favicon.ico"]
+EXTRA_PATH_METADATA = {
+    "extra/robots.txt": {"path": "robots.txt"},
+    "extra/favicon.ico": {"path": "favicon.ico"},
+}
 
 # Blogroll
 LINKS = (
@@ -47,10 +74,19 @@ SOCIAL = (
     ("book fa-lg", "https://www.goodreads.com/user/show/18841479-eric-rochow"),
 )
 
+MAIN_MENU = True
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
 GOOGLE_ANALYTICS = "UA-135617138-1"
 GITHUB_URL = "https://github.com/ericrochow"
+GITHUB_USERNAME = "ericrochow"
+TWITTER_USERNAME = "eric_rochow"
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 10
 
 IGNORE_FILES = [".#*", ".swp", ".tmpl"]
 # Uncomment following line if you want document-relative URLs when developing
